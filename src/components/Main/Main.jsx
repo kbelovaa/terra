ba9e84bf28344dbcce6d3d26d5a2e98e40dff709
Ekaterina Belova <kbelovaa17@gmail.com>
2024-05-01 18:33:19 +0300

    Commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import sendData from '../../http/sendData';
 import img1 from '../../images/img1.png';
 import img2 from '../../images/img2.png';
+import img2m from '../../images/img2_1024.png';
 import img3 from '../../images/img3.png';
 import img4 from '../../images/img4.png';
 import img5 from '../../images/img5.png';
+import img5m from '../../images/img5_1024.png';
 import img6 from '../../images/img6.png';
 import img7 from '../../images/img7.png';
 import img8 from '../../images/img8.png';
 import img9 from '../../images/img9.png';
 import img10 from '../../images/img10.png';
 import img11 from '../../images/img11.png';
+import img11m from '../../images/img11_1024.png';
 import img12 from '../../images/img12.png';
+import img12m from '../../images/img12_1024.png';
 import img13 from '../../images/img13.png';
+import img13m from '../../images/img13_1024.png';
 import img14 from '../../images/img14.png';
+import img14m from '../../images/img14_1024.png';
 import img15 from '../../images/img15.png';
+import img15m from '../../images/img15_1024.png';
 import img16 from '../../images/img16.png';
+import img16m from '../../images/img16_1024.png';
 import img17 from '../../images/img17.png';
 import img18 from '../../images/img18.png';
 import img19 from '../../images/img19.png';
@@ -34,6 +42,20 @@ const Main = () => {
   const [isFormValid, setIsFormValid] = useState(true);
   const [loading, setLoading] = useState(false);
   const [isFormSended, setIsFormSended] = useState(false);
+  const [windowWidth, setWindowWidth] = useState();
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      setWindowWidth(width);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const handleEmailChange = (email) => {
     setEmail(email);
@@ -101,19 +123,63 @@ const Main = () => {
               <div className="image__circle1"></div>
               <div className="image__circle2"></div>
             </div>
-            <svg
-              className="image__arrow"
-              xmlns="http://www.w3.org/2000/svg"
-              width="129"
-              height="175"
-              viewBox="0 0 129 175"
-              fill="none"
-            >
-              <path
-                d="M0.92419 172.889C0.92419 173.543 1.45482 174.074 2.10938 174.074C2.76393 174.074 3.29456 173.543 3.29456 172.889C3.29456 172.234 2.76393 171.704 2.10938 171.704C1.45482 171.704 0.92419 172.234 0.92419 172.889ZM125.815 2C125.815 2.65456 126.345 3.18519 127 3.18519C127.655 3.18519 128.185 2.65456 128.185 2C128.185 1.34544 127.655 0.814815 127 0.814815C126.345 0.814815 125.815 1.34544 125.815 2ZM2.28879 173.02L127.179 2.13112L126.821 1.86888L1.92996 172.758L2.28879 173.02Z"
-                fill="white"
-              />
-            </svg>
+            {windowWidth <= 744 ? (
+              <svg
+                className="image__arrow"
+                xmlns="http://www.w3.org/2000/svg"
+                width="82"
+                height="89"
+                viewBox="0 0 82 89"
+                fill="none"
+              >
+                <path
+                  d="M79.2679 87.2513C79.2679 87.9059 79.7986 88.4365 80.4531 88.4365C81.1077 88.4365 81.6383 87.9059 81.6383 87.2513C81.6383 86.5968 81.1077 86.0662 80.4531 86.0662C79.7986 86.0662 79.2679 86.5968 79.2679 87.2513ZM0.314815 1.5C0.314815 2.15456 0.84544 2.68519 1.5 2.68519C2.15456 2.68519 2.68519 2.15456 2.68519 1.5C2.68519 0.84544 2.15456 0.314815 1.5 0.314815C0.84544 0.314815 0.314815 0.84544 0.314815 1.5ZM80.6166 87.1008L1.66348 1.34948L1.33652 1.65052L80.2896 87.4019L80.6166 87.1008Z"
+                  fill="white"
+                />
+              </svg>
+            ) : windowWidth <= 1024 ? (
+              <svg
+                className="image__arrow"
+                xmlns="http://www.w3.org/2000/svg"
+                width="74"
+                height="69"
+                viewBox="0 0 74 69"
+                fill="none"
+              >
+                <path
+                  d="M0.787471 67.1895C0.787471 67.8441 1.3181 68.3747 1.97266 68.3747C2.62722 68.3747 3.15784 67.8441 3.15784 67.1895C3.15784 66.535 2.62722 66.0043 1.97266 66.0043C1.3181 66.0043 0.787471 66.535 0.787471 67.1895ZM71.3148 2C71.3148 2.65456 71.8454 3.18519 72.5 3.18519C73.1546 3.18519 73.6852 2.65456 73.6852 2C73.6852 1.34544 73.1546 0.814815 72.5 0.814815C71.8454 0.814815 71.3148 1.34544 71.3148 2ZM2.12349 67.3527L72.6508 2.16319L72.3492 1.83681L1.82182 67.0263L2.12349 67.3527Z"
+                  fill="white"
+                />
+              </svg>
+            ) : windowWidth <= 1440 ? (
+              <svg
+                className="image__arrow"
+                xmlns="http://www.w3.org/2000/svg"
+                width="97"
+                height="96"
+                viewBox="0 0 97 96"
+                fill="none"
+              >
+                <path
+                  d="M0.478877 94.0099C0.478877 94.6645 1.0095 95.1951 1.66406 95.1951C2.31862 95.1951 2.84925 94.6645 2.84925 94.0099C2.84925 93.3553 2.31862 92.8247 1.66406 92.8247C1.0095 92.8247 0.478877 93.3553 0.478877 94.0099ZM93.8148 2C93.8148 2.65456 94.3454 3.18519 95 3.18519C95.6546 3.18519 96.1852 2.65456 96.1852 2C96.1852 1.34544 95.6546 0.814815 95 0.814815C94.3454 0.814815 93.8148 1.34544 93.8148 2ZM1.82007 94.1682L95.156 2.15825L94.844 1.84175L1.50806 93.8516L1.82007 94.1682Z"
+                  fill="white"
+                />
+              </svg>
+            ) : (
+              <svg
+                className="image__arrow"
+                xmlns="http://www.w3.org/2000/svg"
+                width="129"
+                height="175"
+                viewBox="0 0 129 175"
+                fill="none"
+              >
+                <path
+                  d="M0.92419 172.889C0.92419 173.543 1.45482 174.074 2.10938 174.074C2.76393 174.074 3.29456 173.543 3.29456 172.889C3.29456 172.234 2.76393 171.704 2.10938 171.704C1.45482 171.704 0.92419 172.234 0.92419 172.889ZM125.815 2C125.815 2.65456 126.345 3.18519 127 3.18519C127.655 3.18519 128.185 2.65456 128.185 2C128.185 1.34544 127.655 0.814815 127 0.814815C126.345 0.814815 125.815 1.34544 125.815 2ZM2.28879 173.02L127.179 2.13112L126.821 1.86888L1.92996 172.758L2.28879 173.02Z"
+                  fill="white"
+                />
+              </svg>
+            )}
           </div>
           <h1 className="info-block__title">Marbella - Terra Brand New Apartment</h1>
           <p className="info-block__text">
@@ -158,10 +224,18 @@ const Main = () => {
           </div>
           <div className="rooms__block"></div>
           <div className="rooms__images">
-            <img src={img2} className="rooms__img_1" alt="Photo" />
+            {windowWidth <= 1440 ? (
+              <img src={img2m} className="rooms__img_1" alt="Photo" />
+            ) : (
+              <img src={img2} className="rooms__img_1" alt="Photo" />
+            )}
             <img src={img3} className="rooms__img_2" alt="Photo" />
             <img src={img4} className="rooms__img_3" alt="Photo" />
-            <img src={img5} className="rooms__img_4" alt="Photo" />
+            {windowWidth <= 1440 ? (
+              <img src={img5m} className="rooms__img_4" alt="Photo" />
+            ) : (
+              <img src={img5} className="rooms__img_4" alt="Photo" />
+            )}
           </div>
         </section>
         <section className="nature">
@@ -200,7 +274,11 @@ const Main = () => {
             </p>
           </div>
           <img src={img10} className="kitchen__img1" alt="Photo" />
-          <img src={img11} className="kitchen__img2" alt="Photo" />
+          {windowWidth <= 1440 ? (
+            <img src={img11m} className="kitchen__img2" alt="Photo" />
+          ) : (
+            <img src={img11} className="kitchen__img2" alt="Photo" />
+          )}
         </section>
         <section className="livingroom">
           <div className="livingroom__block"></div>
@@ -212,7 +290,11 @@ const Main = () => {
               tv/entertainment system.
             </p>
           </div>
-          <img src={img12} className="livingroom__img" alt="Photo" />
+          {windowWidth <= 1440 ? (
+            <img src={img12m} className="livingroom__img" alt="Photo" />
+          ) : (
+            <img src={img12} className="livingroom__img" alt="Photo" />
+          )}
         </section>
         <section className="bedroom">
           <div className="bedroom__block"></div>
@@ -224,12 +306,21 @@ const Main = () => {
               second bathroom also has a rainfall shower. All three bedrooms have fitted wardrobes.
             </p>
           </div>
-          <div className="bedroom__images">
-            <img src={img13} className="bedroom__img1" alt="Photo" />
-            <img src={img14} className="bedroom__img2" alt="Photo" />
-            <img src={img15} className="bedroom__img3" alt="Photo" />
-            <img src={img16} className="bedroom__img4" alt="Photo" />
-          </div>
+          {windowWidth <= 1440 ? (
+            <div className="bedroom__images">
+              <img src={img13m} className="bedroom__img1" alt="Photo" />
+              <img src={img14m} className="bedroom__img2" alt="Photo" />
+              <img src={img15m} className="bedroom__img3" alt="Photo" />
+              <img src={img16m} className="bedroom__img4" alt="Photo" />
+            </div>
+          ) : (
+            <div className="bedroom__images">
+              <img src={img13} className="bedroom__img1" alt="Photo" />
+              <img src={img14} className="bedroom__img2" alt="Photo" />
+              <img src={img15} className="bedroom__img3" alt="Photo" />
+              <img src={img16} className="bedroom__img4" alt="Photo" />
+            </div>
+          )}
         </section>
         <section className="terraces">
           <div className="terraces__info info">
--- src/constants/weekdays.js
+++
@@ -1,3 +0,0 @@
-const weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-
-export default weekdays;
