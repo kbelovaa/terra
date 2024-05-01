f96c3f629d6448d94f513e89eb810608a528cb94
Ekaterina Belova <kbelovaa17@gmail.com>
2024-05-01 12:56:22 +0300

    Commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import ScrollToTop from '../ScrollToTop/ScrollToTop';
 import Main from '../Main/Main';
-import '../../utils/i18n';
 import './App.scss';
 
 const App = () => (
